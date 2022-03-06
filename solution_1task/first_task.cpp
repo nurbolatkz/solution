126dfe38d95fce2d8a26b36604011a722c20b945
Nurbolat <nurbolat.duisenbek@narxoz.kz>
2022-03-07 01:46:17 +0600

    use define

--- solution_1task/first_task.cpp
+++ solution_1task/first_task.cpp
@@ -1,18 +1,14 @@
-
 #include <iostream>
 #include <iomanip>
 #include "helper.h"
 using namespace std;
-
-
 
 int main()
 {
     int DIM1 = 3, DIM2 = 3, DIM3 = 3;
     int ***ary =  arraycreator(DIM1, DIM2, DIM3);
 
-    vector<vector<int>> myresult = searchKvalue(ary, 3,3,3,3);
-
+    vector<vector<int>> myresult = searchKvalue(ary, size_x, size_y, k_val);
 
     for(int i = 0; i < myresult.size(); i++){
 
